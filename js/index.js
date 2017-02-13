fe96c0115453a2b9156d7e99c4e7761cd5f583fe
Davide Gessa <gessadavide@gmail.com>
2017-02-13 22:19:43 +0100

    site bump

--- js/index.js
+++ js/index.js
@@ -5,15 +5,24 @@ ligApp.controller('indexController', function indexController($scope, $http) {
     $scope.contributions = [];
     $scope.authors = {};
     $scope.events = [];
+    $scope.lignode = "liskwallet.punkrock.me"
     $scope.fulig = {
         address: '13861531827625059307L',
         balance: null
     };
 
-    $http.get ('http://51.15.52.112:8000/api/accounts/getBalance?address=' + $scope.fulig.address).then (function (data) {
+    $http.get ('https://' + $scope.lignode + '/api/accounts/getBalance?address=' + $scope.fulig.address).then (function (data) {
         $scope.fulig.balance = parseInt (data.data.balance) / 100000000;
     });
-
+    $http.get ('https://' + $scope.lignode + '/api/transactions?limit=3&recipientId=' + $scope.fulig.address).then (function (data) {
+        console.log("Income");
+        console.log(data.data.transactions);
+        $scope.income = data.data.transactions;
+    });
+    $http.get ('https://' + $scope.lignode + '/api/transactions?limit=3&senderId=' + $scope.fulig.address).then (function (data) {
+        console.log(data.data.transactions);
+        $scope.outcome = data.data.transactions;
+    });
     $http.get ('data/projects.json').then (function (data) {
         $scope.projects = data.data;
     });
