a68e695a4529b4a703b7d02ef0199fcc0b3a4a6c
Davide Gessa <gessadavide@gmail.com>
2017-02-24 09:23:29 +0100

    site bump

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var ligApp = angular.module ('ligApp', []);
 
 ligApp.controller('indexController', function indexController($scope, $http) {
+	$scope.epochTime = new Date(Date.UTC(2016, 4, 24, 17, 0, 0, 0)).getTime();
     $scope.income = []
     $scope.outcome = []
     $scope.projects = [];
@@ -34,24 +35,35 @@ ligApp.controller('indexController', function indexController($scope, $http) {
             $scope.income.push(e)
         }, this)
     })
-    $http.get('https://' + $scope.lignode + '/api/transactions?limit=3&senderId=' + $scope.fulig.address).then(function (data) {
-        console.log('Outcome')
-        console.log(data.data.transactions)
-        let tempOutcome = data.data.transactions
+    
+    $http.get ('data/outcomes.json').then (function (outdict) {
+    	outdict = outdict.data;
+    	
+		$http.get('https://' + $scope.lignode + '/api/transactions?limit=3&senderId=' + $scope.fulig.address).then(function (data) {
+		    console.log('Outcome')
+		    console.log(data.data.transactions)
+		    let tempOutcome = data.data.transactions
 
-        tempOutcome.forEach(function (e) {
-            $http.get('https://' + $scope.lignode + '/api/accounts/getPublicKey?address=' + e.recipientId).then(function (data) {
-                if (data.data.success && data.status === 200) {
-                    $http.get('https://' + $scope.lignode + '/api/delegates/get?publicKey=' + data.data.publicKey).then(function (data) {
-                        if (data.data.success && data.status === 200) {
-                            e.delegate = data.data.delegate.username
-                        }
-                    })
-                }
-            })
-            $scope.outcome.push(e)
-        }, this)
-    })
+		    tempOutcome.forEach(function (e) {
+		    	if (e.id in outdict)
+		    		e.description = outdict[e.id].description;
+		    		
+		    	e.timestamp = parseInt (e.timestamp) * 1000 + $scope.epochTime;
+		    		
+		        $http.get('https://' + $scope.lignode + '/api/accounts/getPublicKey?address=' + e.recipientId).then(function (data) {
+		            if (data.data.success && data.status === 200) {
+		                $http.get('https://' + $scope.lignode + '/api/delegates/get?publicKey=' + data.data.publicKey).then(function (data) {
+		                    if (data.data.success && data.status === 200) {
+		                        e.delegate = data.data.delegate.username
+		                    }
+		                })
+		            }
+		        })
+		        $scope.outcome.push(e);
+		    }, this);
+		})
+	});
+	
     $http.get ('data/projects.json').then (function (data) {
         $scope.projects = data.data;
     });
@@ -71,4 +83,9 @@ ligApp.controller('indexController', function indexController($scope, $http) {
     $http.get ('data/members.json').then (function (data) {
         $scope.members = data.data;
     });
+    
+    $scope.outcomeDetails = function (o) {
+    	$scope.selectedOutcome = o;
+    	$('#outcomeDetailsModal').modal ('show');
+    };
 });
