3a73bcb2098b0ebd0d144cab2d3684ed64afff29
Andrea Baccega <vekexasia@gmail.com>
2017-09-26 08:27:55 +0200

    modifiche al faucet HTML

--- src/pages/FaucetPage.vue
+++ src/pages/FaucetPage.vue
@@ -27,40 +27,47 @@
         <li>Have at least {{faucetConfig.minWalletAmount}} LSK in your address</li>
       </ul>
       <p>
-        Every ~4 weeks you'll be able to receive up to 1LSK if you maintained the requirements mentioned above for the whole period.</p>
+        Every ~1.25 weeks you'll be able to receive up to 1LSK if you maintained the requirements mentioned above for the whole period.</p>
       <p>Use the form below to check requirements and request a withdraw.</p>
       <md-divider></md-divider>
-      <md-layout>
-        <md-layout md-flex="60">
-          <md-input-container v-bind:class="{'md-input-invalid':!validAddress && userAddress != ''}">
-            <label>Your Address</label>
-            <md-input v-model="userAddress"></md-input>
-            <span class="md-error">Address is invalid!</span>
-          </md-input-container>
-        </md-layout>
-        <md-layout md-flex="40" md-align="center">
-          <div style="margin-left: auto;">
-            <md-button class="md-raised md-primary"
-                       @click.native="doWithdraw()"
-                       style="margin-top:10px;" :disabled="!validAddress || withdrawing || checking">
-              Get Reward
-            </md-button>
-          </div>
+      <md-layout style="margin-top:24px">
+        <md-layout md-flex="25" md-flex-xsmall="0"></md-layout>
+        <md-layout md-flex="50" md-flex-xsmall="100">
+          <md-whiteframe>
+            <md-layout>
+              <md-layout md-flex="60">
+                <md-input-container v-bind:class="{'md-input-invalid':!validAddress && userAddress != ''}">
+                  <label>Your Address</label>
+                  <md-input v-model="userAddress"></md-input>
+                  <span class="md-error">Address is invalid!</span>
+                </md-input-container>
+              </md-layout>
+              <md-layout md-flex="40" md-align="center">
+                <div style="margin-left: auto;">
+                  <md-button class="md-raised md-primary"
+                             @click.native="doWithdraw()"
+                             style="margin-top:10px;" :disabled="!validAddress || withdrawing || checking">
+                    Get Reward
+                  </md-button>
+                </div>
+              </md-layout>
+            </md-layout>
+            <md-progress md-indeterminate v-if="checking"/>
+            <div v-if="!checking">
+              <span v-if="error"><md-icon>warning</md-icon> {{error}}</span>
+            </div>
+            <div v-if="withdrawing">
+              <p>Preparing your faucet reward. You'll receive {{calculatedReward}} LSK</p>
+              <md-progress md-indeterminate/>
+            </div>
+            <div v-else-if="withdrawRes">
+              <span v-if="withdrawRes.err"><md-icon>warning</md-icon> {{withdrawRes.msg}}</span>
+              <span v-else><md-icon>check</md-icon> Transaction successful! -> <a target="_blank"
+                  :href="viewUtils.explorerTxLink(withdrawRes.txID)">{{withdrawRes.txID}}</a></span>
+            </div>
+          </md-whiteframe>
         </md-layout>
       </md-layout>
-      <md-progress md-indeterminate v-if="checking"/>
-      <div v-if="!checking">
-        <span v-if="error"><md-icon>warning</md-icon> {{error}}</span>
-      </div>
-      <div v-if="withdrawing">
-        <p>Preparing your faucet reward. You'll receive {{calculatedReward}} LSK</p>
-        <md-progress md-indeterminate/>
-      </div>
-      <div v-else-if="withdrawRes">
-        <span v-if="withdrawRes.err"><md-icon>warning</md-icon> {{withdrawRes.msg}}</span>
-        <span v-else><md-icon>check</md-icon> Transaction successful! -> <a
-            :href="viewUtils.explorerTxLink(withdrawRes.txID)">{{withdrawRes.txID}}</a></span>
-      </div>
 
 
     </section>
@@ -71,7 +78,7 @@
 <script lang="ts" src="./FaucetPage.ts"/>
 <style lang="scss">
   .poolpage {
-    padding-bottom: 96px;
+    padding-bottom: 250px;
     img.logo {
       width: 108px;
       height: 150px;
